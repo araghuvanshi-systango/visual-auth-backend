731a621149ee82dc17179a36d5154b55920274b3
bhavesha-systango <bagnihotri@isystango.com>
2022-06-30 17:50:32 +0530

    added the video capturing and frame functionality

--- visual_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from visual_auth import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.visual_feed)
-]
--- visual_auth/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def visual_feed(request):
-    return render(request, 'visual_feed.html')
