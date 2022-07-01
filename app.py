ca914592ed8f47321be8314cb4acb528981d572c
Abhay Raghuvanshi <araghuvanshi@isystango.com>
2022-07-01 12:07:33 +0530

    Refactoring code and removing unsed code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def generate_frames():
         if not success:
             break
         else:
-            ret,buffer=cv2.imencode('.jpg',frame)
+            ret, buffer=cv2.imencode('.jpg',frame)
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
@@ -28,9 +28,7 @@ def index():
 
 @app.route('/video')
 def video():
-    print("hello")
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
     app.run(debug=True)
-
